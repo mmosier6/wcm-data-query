ea64be6f3efad206b8cc1b45a6b0eb9290684444
ebentley52 <bentleys@Evans-MBP.attlocal.net>
2020-03-25 20:57:37 -0500

    update javascript

--- wcm/dataquery/js/page.js
+++ wcm/dataquery/js/page.js
@@ -1,4 +1,5 @@
 function buildPage(page){
+	page.reportType="tornado_reports";
 	//Data type buttonset
 	jQuery("#data-type-buttonset").buttonset().change(function(){
 		var el = jQuery(this);
@@ -8,11 +9,12 @@ function buildPage(page){
 				v = jQuery(this).attr("value");
 			}
 		});
-		page.dataType = v;
+		page.dataType = v;	
 		if(v === 'watch'){
 			jQuery("#all-watch-filters").removeClass("hidden");
 			jQuery("#all-report-filters").addClass("hidden");
-
+			jQuery("#report-type").addClass("hidden");
+			
 			if(jQuery("#date-pickers").hasClass("hidden")){
 				jQuery("#date-pickers").removeClass("hidden");
 			}
@@ -24,15 +26,31 @@ function buildPage(page){
 		if(v === 'report'){
 			jQuery("#all-watch-filters").addClass("hidden");
 			jQuery("#all-report-filters").removeClass("hidden");
+			jQuery("#report-type").removeClass("hidden");
+			jQuery("#data-type-5").prop('checked', true);
+			jQuery("#data-type-5").addClass("ui-state-active")
+			jQuery("#data-type-5").button('refresh');
 			if(jQuery("#date-pickers").hasClass("hidden")){
 				jQuery("#date-pickers").removeClass("hidden");
-			}
+			}		
 			if(jQuery("#filter-opt-list").hasClass("hidden")){
 				jQuery("#filter-opt-list").removeClass("hidden");
 			}
-		}
+		}	
+
 	});
 
+	jQuery("#data-type-buttonset1").buttonset().change(function(){
+		var ele = jQuery(this);
+		var w = '';
+		ele.find("input:radio").each(function(){
+			if(jQuery(this).prop("checked")){
+				w = jQuery(this).attr("value");
+			}
+		});
+		page.reportType = w;
+	});
+	
 	//Create dialog box
 	jQuery("#error-dialog").dialog({
 		autoOpen: false,
@@ -49,32 +67,34 @@ function buildPage(page){
 			}
 		}
 	});
-
+	
 	createCalendars(page);
-
+	
 	filterStates(page);
 	filterCWAs(page);
 	filterFIPSandZIP(page);
-
-	//Get watch data
-	jQuery.ajax({
-		dataType: "json",
-		url: "/wcm/data/raw/watches_all_2019.json"
-	}).done(function(data){
-		page.data = new Object();
-		page.data['all'] = data;
-		createFilteredData(page);
-	});
-
+	
 	jQuery("#go-btn").button().on('click', function(){
 		jQuery("#data-type-buttonset input:radio").each(function(){
 			if(jQuery(this).prop("checked")){
 				v = jQuery(this).attr("value");
 				page.dataType = v;
 			}
-		});
-
+		});	
+		if (page.dataType ==="watch") {
+		var urlStr ="/wcm/data/raw/watches_all_2019.json"
+		} else if (page.dataType ==="report") {
+		var urlStr ="/wcm/data/raw/"+page.reportType+".json"
+		}
+			//Get watch data
+	jQuery.ajax({
+		dataType: "json",
+		url: urlStr
+	}).done(function(data){
+		page.data = new Object();
+		page.data['all'] = data;
+		createFilteredData(page);
 		getFilteredData(page);
-
+	});	
 	});
 }
--- wcm/dataquery/js/tables.js
+++ wcm/dataquery/js/tables.js
@@ -1,7 +1,6 @@
 var finalJSON="";
 function displayFilteredData(page){
-	console.log("displayFilteredData");
-	console.log(page.data['filtered'])
+if (page.dataType === "watch") {
 	
 	function compare(a,b){
 		const watchA = a.wnum;
@@ -84,11 +83,179 @@ function displayFilteredData(page){
 	tot = tot + "<td id='results_cell'>"+totalPDSTornado+"</td>";
 	tot = tot + "</tr>";
 	tot = tot + "</table>";
+} 
+else if (page.dataType === "report") {
 
+	function compare(a,b){
+		const ReportA = a.date;
+		const ReportB = b.date;
+		const TimeA = a.time;
+		const TimeB = b.time;
+		
+		var comparison = 0;
+		if(ReportA > ReportB){
+			comparison = 1;
+		}else if(ReportA < ReportB){
+			comparison = -1;
+		}else if (ReportA === ReportB){
+			if (TimeA > TimeB) {
+				comparison = 1;
+			} else {
+				comparison = -1;
+			}
+		}
+		
+		return comparison;
+	}
+
+	page.data['filtered-sorted'] = page.data['filtered'].slice().sort(compare);		
+
+	var total=0;
+	var total0=0;
+	var total1=0;
+	var total2=0;
+	var total3=0;
+	var total4=0;
+	var total5=0;
+	var DataTableTitle1 = "Date/Time";
+	var DataTableTitle2 = "";
+	var TotalTitle0 = "";
+	var TotalTitle1 = "";
+	var TotalTitle2 = "";
+	var TotalTitle3 = "";
+	var TotalTitle4 = "";
+	var TotalTitle5 = "";
+
+	if (page.reportType === "tornado_reports") {
+	DataTableTitle2 = "Tornado Rating";
+	TotalTitle0 = "EF0";
+	TotalTitle1 = "EF1";
+	TotalTitle2 = "EF2";
+	TotalTitle3 = "EF3";
+	TotalTitle4 = "EF4";
+	TotalTitle5 = "EF5";		
+	} else if (page.reportType === "hail_reports") {
+	DataTableTitle2 = "Hail Size";
+	TotalTitle0 = "<1";
+	TotalTitle1 = '1"-1.99"';
+	TotalTitle2 = '2"-2.99"';
+	TotalTitle3 = '3"-3.99"';
+	TotalTitle4 = '4"-4.99"';
+	TotalTitle5 = '5+"';
+	} else if (page.reportType === "wind_reports") {
+	DataTableTitle2 = "Wind Speed";
+	TotalTitle0 = "50-59 kts";
+	TotalTitle1 = "60-69 kts";
+	TotalTitle2 = "70-79 kts";
+	TotalTitle3 = "80-89 kts";
+	TotalTitle4 = "90-99 kts";
+	TotalTitle5 = "100+ kts";
+	}
+	var dataKeys = new Array("om", "date", "mag", "time");
+	var st = "";
+	st = st + "<table id='results_table' width = '100%'>";
+	st = st + "<tr>";
+	st = st + "<th id='results_cell'>"+DataTableTitle1+"</th>";
+	st = st + "<th id='results_cell'>"+DataTableTitle2+"</th>";
+	st = st + "</tr>";
+	page.data['filtered-sorted'].forEach(function(d,n){
+		var fulldt = d["date"];
+		var fulltime = d["time"];
+		st = st + "<tr>";
+		st = st + "<td id='results_cell'> " + fulldt.slice(4,6) +"/"+ fulldt.slice(6,8) +"/"+ fulldt.slice(0,4) +" "+ fulltime.slice(0,5) +  " CST</td>";
+		st = st + "<td id='results_cell'> " + d['mag'] + "</td>";
+		st = st + "</tr>";
+		total=total+1;
+	if (page.reportType === "tornado_reports") {
+		if (d['mag']===0) {
+			total0=total0+1;
+		}	else if (d['mag']===1) {
+			total1=total1+1;
+		}	else if (d['mag']===2) {
+			total2=total2+1;
+		}	else if (d['mag']===3) {
+			total3=total3+1;
+		}	else if (d['mag']===4) {
+			total4=total4+1;
+		}	else if (d['mag']===5) {
+			total5=total5+1;
+		}
+	} else if (page.reportType === "wind_reports") {
+		if (d['mag']>49 & d['mag']<60) {
+			total0=total0+1;
+		}	else if (d['mag']>59 & d['mag']<70) {
+			total1=total1+1;
+		}	else if (d['mag']>69 & d['mag']<80) {
+			total2=total2+1;
+		}	else if (d['mag']>79 & d['mag']<90) {
+			total3=total3+1;
+		}	else if (d['mag']>89 & d['mag']<100) {
+			total4=total4+1;
+		}	else if (d['mag']>99 & d['mag']<200) {
+			total5=total5+1;
+		}
+	} else if (page.reportType === "hail_reports") {
+		if (d['mag']>0 & d['mag']<1) {
+			total0=total0+1;
+		}	else if (d['mag']>.99 & d['mag']<2) {
+			total1=total1+1;
+		}	else if (d['mag']>1.99 & d['mag']<3) {
+			total2=total2+1;
+		}	else if (d['mag']>2.99 & d['mag']<4) {
+			total3=total3+1;
+		}	else if (d['mag']>3.99 & d['mag']<5) {
+			total4=total4+1;
+		}	else if (d['mag']>4.99 & d['mag']<10) {
+			total5=total5+1;
+		}
+	}		
+		finalJSON = finalJSON + ',{"wnum":"'+d["wnum"]+'","ST":["'+d["ST"]+'"],"FIPS":["'+d["FIPS"]+'"],"issue_dt":"'+d["issue_dt"]+'","CWA":["'+d["CWA"]+'"],"type":"'+d["type"]+'"}';
+	});	
+
+	st = st + "</table>";
+
+	tot = "";
+	tot = tot + "<table id='total_table'>";
+	tot = tot + "<tr>";
+	tot = tot + "<th id='results_cell'></th>";
+	tot = tot + "<th id='results_cell'> Total </th>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle0+"</td>";
+	tot = tot + "<td id='results_cell'>"+total0+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle1+"</td>";
+	tot = tot + "<td id='results_cell'>"+total1+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle2+"</td>";
+	tot = tot + "<td id='results_cell'>"+total2+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle3+"</td>";
+	tot = tot + "<td id='results_cell'>"+total3+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle4+"</td>";
+	tot = tot + "<td id='results_cell'>"+total4+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>"+TotalTitle5+"</td>";
+	tot = tot + "<td id='results_cell'>"+total5+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "<tr>";
+	tot = tot + "<td id='results_cell'>Total</td>";
+	tot = tot + "<td id='results_cell'>"+total+"</td>";
+	tot = tot + "</tr>";
+	tot = tot + "</table>";
+}
 	
 	jQuery("#data-table").empty();
 	
 	jQuery("#data-table").html(st);
+
+	jQuery("#total-table").empty();
 
 	jQuery("#total-table").html(tot);
 
